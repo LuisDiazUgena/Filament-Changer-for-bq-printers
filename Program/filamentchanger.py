e098ece939be32451111dd450a646523bf48cfa0
Luis Diaz <luisdiazugena@gmail.com>
2016-12-28 01:04:39 +0100

    Added functionality to the program
    Conected slots and signals, created functions for save, browse and
    export buttons. Also, created the first script to look for the keyword
    "layer" in the file

--- Program/filamentchanger.py
+++ Program/filamentchanger.py
@@ -36,19 +36,66 @@ class FilamentChanger(QtGui.QWidget):
         layout.addWidget(main_widget)
         self.setLayout(layout)
         self.setWindowTitle("Filament Changer for Marlin 3D Printers")
+
         #Get a reference to all required widgets
-        self.BrowseButton = self.findChild(QtGui.QToolButton,'BrowseButton')
         self.FileLineEdit = self.findChild(QtGui.QLineEdit,'FileLineEdit')
         self.LayerSpinBox = self.findChild(QtGui.QSpinBox,'LayerSpinBox')
         self.EnabledCheckBox = self.findChild(QtGui.QCheckBox,'EnabledCheckBox')
 
-        self.ExportBtn = self.findChild(QtGui.QToolButton,'ExportBtn')
-        self.ResetBtn = self.findChild(QtGui.QToolButton,'ResetBtn')
+        self.ExportBtn = self.findChild(QtGui.QPushButton,'ExportBtn')
+        self.ResetBtn = self.findChild(QtGui.QPushButton,'ResetBtn')
+        self.BrowseButton = self.findChild(QtGui.QPushButton,'BrowseButton')
+
         #Configure widget ranges
-        #Todo: Change the max value to the last layer of the script.
+        #TODO: Change the max value to the last layer of the script.
         #max_layer = sys.float_info.max
         #self.LayerSpinBox.setMaximum(max_layer)
 
+        #Connect slot and signals
+        self.EnabledCheckBox.stateChanged.connect(self.onEnabledCheckBoxChangedState)
+        self.ExportBtn.clicked.connect(self.onExportBtnClicked)
+        self.ResetBtn.clicked.connect(self.onResetBtnClicked)
+        self.BrowseButton.clicked.connect(self.OnBrowseButtonClicked)
+
+    def onSaveButtonClicked(self):
+        reply = QtGui.QMessageBox.question(parent=self, title='Attention',
+                                           text='File will be overwritten.\nDo you still want to proceed?',
+                                           buttons=QtGui.QMessageBox.Yes | QtGui.QMessageBox.No,
+                                           defaultButton=QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            print("response is yes")
+    def OnBrowseButtonClicked(self):
+        print("OnBrowseButtonClicked")
+        #TODO Create browse routine
+        filename, filter = QtGui.QFileDialog.getOpenFileName(parent=self, caption='Open file', dir='.', filter='gcode files(*.gcode)')
+        if filename:
+            self.FileLineEdit.setText(filename)
+            LookForLayers(filename)
+    def onExportBtnClicked(self):
+        print("onExportBtnClicked")
+        #TODO Create export routine
+        filename, filter = QtGui.QFileDialog.getSaveFileName(parent=self, caption='Select output file', dir='.', filter='*.gcode')
+        if filename:
+            if '.gcode' != filename[-6]:
+                filename += '.gcode'
+
+            print("File saved!")
+    def onResetBtnClicked(self):
+        print("onResetBtnClicked")
+        #TODO Create reset routine
+
+    def onEnabledCheckBoxChangedState(self, checked):
+        self.EnabledCheckBox.setChecked(bool(checked))
+def LookForLayers(filename):
+    filename = os.path.abspath(os.path.realpath(filename))
+    datafile = open(filename,'r')
+    count = 0
+    for line in datafile:
+        if 'layer' in line:
+            count += 1
+
+    print("Found layer " + repr(count) +" times")
 if __name__ == '__main__':
 
     # Create Qt app
